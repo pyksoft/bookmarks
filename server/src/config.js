5a52129f3063aea5af1148a865a140e84cf58542
pyksoft <pyksoft@gmail.com>
2016-11-04 17:36:27 +1000

    removed auto generated server js/map files

--- server/src/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports["default"] = {
-    port: 4080,
-    isDevLocal: true
-};
-//# sourceMappingURL=config.js.map
--- server/src/startServer.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-process.on('uncaughtException', function (err) {
-    var stack = err.stack;
-    console.log("Uncaught exception. " + err);
-});
-var server_1 = require('./server');
-server_1["default"].start({});
-//# sourceMappingURL=startServer.js.map
