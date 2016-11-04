d561fc5310a069209cd1cc3c90e85ca6d305c9e1
pyksoft <pyksoft@gmail.com>
2016-11-04 14:22:54 +1000

    add typings to repo

---
+++ server/typings/modules.d.ts
@@ -0,0 +1,24 @@
+declare module 'winston' {
+    var x: any;
+    export = x;
+}
+
+declare module 'express' {
+    var x: any;
+    export = x;
+}
+
+declare module 'body-parser' {
+    var x: any;
+    export = x;
+}
+
+declare module 'cors' {
+    var x: any;
+    export = x;
+}
+
+declare module 'lodash' {
+    var x: any;
+    export = x;
+}
