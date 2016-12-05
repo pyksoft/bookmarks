984c1389b04679e6f08fa1b998d0a6ff2884c96c
pyksoft <pyksoft@gmail.com>
2016-12-05 18:47:23 +0700

    help content update; scroll for help

--- client/src/components/help/help.md.js
+++ client/src/components/help/help.md.js
@@ -4,21 +4,29 @@ export default
 
 ## Introduction
 
-Bookmarks Archive (*BA*) is desktop program to store your Web Bookmarks. Over time number of bookmarks, which you save for later use or which you want to read later will grow and it is not convenient to keep them in browser bookmarks.
+Bookmarks Archive (*BA*) is a desktop program to store your Web Bookmarks. Over time number of bookmarks saved in your browser will grow and it is not very convenient to manage bookmarks collection in the browser itself.
 
-*BA* allows to manage your bookmarks in easy and convenient way.
+*BA* provides an easy and convenient way to work with your bookmarks collection no matter how big it is.
 
 ## Functionality
 
-You can import bookmarks from any major browser. First export bookmarks as HTML file. Then use that HTML file to import bookmarks into *BA*. You can import bookmarks like that a few times, each bookmark will be imported just once.
+You can import bookmarks from any major browser. First export bookmarks as HTML file. Then use that HTML file to import bookmarks into *BA*. You can import bookmarks in that way a few times, each bookmark will be imported just once.
 
-If bookmark is located is some sub-folder this information will be saved in *original path* field in bookmark.
+If a bookmark is located is some sub-folder this information will be saved for that bookmark in *original path*.
 
-You can edit bookmarks, create tags, assign tags to bookmarks.
+You can edit bookmarks, create tags, assign tags to bookmarks. Tags will allow you to search for bookmarks later on.
 
-There are different filtering modes. You can limit bookmarks to new ones (with no tags), bookmarks with particular tags (or condition) or deleted bookmarks.
+There are different filtering modes. You can search for new ones (with no tags), bookmarks with particular tags (at least one of selected) or deleted bookmarks.
 
-For each filter mode you can search by text. It will search in bookmark *title*, *url* and *original path*.
+For each filter mode, you can search by text. It will search in bookmark *title*, *url* and *original path*.
+
+## Settings
+
+There are a few settings which you can change. Currently it should be done manually by editing file '/data/config/settings.json'. 
+
+You can change port on which program will run. That makes sense only if that port is occupied by something else.
+
+Bookmarks data is stored as JSON file in '/data/db.json' (by default), you can change that location to something else. Make sure you have valid JSON in settings file, for path you have to escape '/' in Windows, for example 'C://Docs//bookmarks.json'.
 
 ## Tips
 
@@ -27,7 +35,7 @@ There are some keyboard shortcuts:
 * Ctrl+Enter: to save bookmark and close popup modal
 * Esc: close modal or close tag list selection
 
-It maybe convenient to use tags names like folder structure:
+It may be convenient to use tags names like folder structure:
 
 * Programming/JavaScript
 * Programming/JavaScript/ES6
